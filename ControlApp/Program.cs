e25e9662f6ac0ff095585cf656d4da8ed0cf9c9e
janyo <ysrg2015@gmail.com>
2025-08-28 07:04:11 -0500

    Diagrama de clases 1.0

--- ControlApp/Program.cs
+++ ControlApp/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public class Productos
+{
+    public int ID_P = 0;
+    public string? nombreProducto = "";
+    public decimal precioProducto = 0.0m;
+    public int iva = 0;
+    public string marca="";
+   
+    public int cantidadProducto = 0; 
+    
+     
+
+}
+
+
+
+public class FacturaProductos : Productos
+{
+    public double Total = 0;
+    
+    public int NumFactura = 0;
+   
+    
+
+}
+public class Tienda
+{
+
+
+    public string? NIT = "";
+    public string Direccion = "";
+    public string? nombre = "";
+
+
+}
+
+public class PropietarioTienda:Tienda
+{
+
+   
+     public string? cedula = "";
+    
+    public string? nombre = "";
+
+
+}
